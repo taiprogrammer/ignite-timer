61b9e9cda75fe423fafa5f93671ef243e733e0ac
taiprogrammer <taiza.marques@sptech.school>
2022-10-07 19:28:31 -0300

    adding a default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Router } from "./Router";
+
 import { GlobalStyle } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default.styles";
 
-import { Button } from "./components/Button";
-
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
       <GlobalStyle />
+      <Router />
     </ThemeProvider>
   );
 }
