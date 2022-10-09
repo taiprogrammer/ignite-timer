0c01c6dd7946346968b8ecb7e6957e5f98e676ad
taiprogrammer <taiza.marques@sptech.school>
2022-10-09 17:12:24 -0300

    chore (Home) - inputs elements and datalist

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,13 +18,21 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
